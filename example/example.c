0bc05909be72f0f394792ceca17b5ed3ecea8b62
Tobias Rehbein <tobias.rehbein@web.de>
2011-11-25 00:27:01 +0100

    Remove redundant prototypes

--- example/example.c
+++ example/example.c
@@ -5,12 +5,6 @@
 
 struct pron_context;
 
-struct pron_context	*pron_context_open(size_t _width, size_t height);
-size_t			 pron_pronisate(struct pron_context *_ctx, char *_filename);
-unsigned char		*pron_get_stream(struct pron_context *_ctx);
-void			 pron_context_close(struct pron_context *_ctx);
-
-
 #define IMAGE	"happy-smiley.gif"
 #define WIDTH	70
 #define HEIGHT	32
